da90bfe05c9fecb42ff1591cad0dcc52da66a3c8
P461066 <pabalelojantjie2@gmail.com>
2026-09-18 23:35:51 +0200

    my first code

---
+++ practice.py
@@ -0,0 +1,12 @@
+n1 = int(input())
+n2 = int(input())
+op = input()
+if op == '+':
+    result = n1 + n2
+elif op == '-':
+    result = n1 - n2
+elif op == '*':
+    result = n1 * n2
+elif op == '/':
+    result = n1 / n2
+print(f"result: {result}")
