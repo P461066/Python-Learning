0d685b290d0b2a4fd0679ad6e0afb60d20805b69
Pablo J <pabalelojantjie2@gmail.com>
2026-09-24 18:58:43 +0200

    Remove calculator functionality from practice.py

--- practice.py
+++ practice.py
@@ -1,12 +1,1 @@
-n1 = int(input())
-n2 = int(input())
-op = input()
-if op == '+':
-    result = n1 + n2
-elif op == '-':
-    result = n1 - n2
-elif op == '*':
-    result = n1 * n2
-elif op == '/':
-    result = n1 / n2
-print(f"result: {result}")
+
